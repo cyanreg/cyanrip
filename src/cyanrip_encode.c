9297ba28aa383d2453a9ea4b0ad8c512c2edc3c1
Rostislav Pehlivanov <atomnuker@gmail.com>
2017-05-13 05:30:25 +0100

    Don't insert empty metadata

--- src/cyanrip_encode.c
+++ src/cyanrip_encode.c
@@ -107,14 +107,20 @@ static void set_metadata(cyanrip_ctx *ctx, cyanrip_track *t, AVFormatContext *av
 
     strftime(t_disc_date, sizeof(t_disc_date), "%FT%H:%M:%S", ctx->disc_date);
 
-    av_dict_set    (&avf->metadata, "comment",            "cyanrip",         0);
-    av_dict_set    (&avf->metadata, "title",              t->name,           0);
-    av_dict_set_int(&avf->metadata, "track",              t->index + 1,      0);
-    av_dict_set    (&avf->metadata, "creation_time",      t_s,               0);
-    av_dict_set    (&avf->metadata, "album",              ctx->disc_name,    0);
-    av_dict_set    (&avf->metadata, "album_artist",       ctx->album_artist, 0);
-    av_dict_set    (&avf->metadata, "date",               t_disc_date,       0);
-    av_dict_set    (&avf->metadata, "musicbrainz_discid", ctx->discid,       0);
+#define ADD_TAG(dict, name, source, flags)          \
+    do {                                            \
+        if (strlen(source))                         \
+            av_dict_set(dict, name, source, flags); \
+    } while (0)
+
+    ADD_TAG(&avf->metadata, "comment",            "cyanrip",         0);
+    ADD_TAG(&avf->metadata, "title",              t->name,           0);
+    ADD_TAG(&avf->metadata, "creation_time",      t_s,               0);
+    ADD_TAG(&avf->metadata, "album",              ctx->disc_name,    0);
+    ADD_TAG(&avf->metadata, "album_artist",       ctx->album_artist, 0);
+    ADD_TAG(&avf->metadata, "date",               t_disc_date,       0);
+    ADD_TAG(&avf->metadata, "musicbrainz_discid", ctx->discid,       0);
+    av_dict_set_int(&avf->metadata, "track",      t->index + 1,      0);
 }
 
 int cyanrip_encode_track(cyanrip_ctx *ctx, cyanrip_track *t,
